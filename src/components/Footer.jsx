c3fcfa970d68577225eac39e66f4704bd120d6c4
nelfimov <nelfimov@mail.ru>
2022-11-20 20:20:10 +0300

    added github, fixes to images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import ICONS_LIST from '../icons.json';
 import '../styles/Footer.css';
 
 const Footer = () => {
   return (
     <footer>
-      <a href="https://github.com/nelfimov">github</a>
+      <a href="https://github.com/nelfimov">
+        <img
+          src={`${process.env.PUBLIC_URL}${ICONS_LIST.general.github}`}
+          alt="github"
+        />
+      </a>
     </footer>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,13 +6,15 @@ const Hero = () => {
   return (
     <section className="hero">
       <a className='anchor' id='hero'></a>
-      <div className="name">
-        <h1>Nikita</h1>
-        <h1>Elfimov</h1>
-      </div>
-      <div className='prof'>
-        <h2>Web</h2>
-        <h2>Developer</h2>
+      <div className="info">
+        <div className="name">
+          <h1>Nikita</h1>
+          <h1>Elfimov</h1>
+        </div>
+        <div className='prof'>
+          <h2>Web</h2>
+          <h2>Developer</h2>
+        </div>
       </div>
       <picture>
         <source
