1018a166342b68f64afe0b0a9c1a06f99e663c4a
nelfimov <nelfimov@mail.ru>
2022-11-20 13:36:53 +0300

    about text ammendments

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,10 +23,25 @@ const About = () => {
       <h1>About me</h1>
       <div>
         <p>
-          A radiophysics student passionately learning Web Development.
-          Started out as student of Yandex Praktikum in Back End, however the
-          study format did not suit me. Continued with my pet project and
-          learning in parallel <a href="https://theodinproject.com">The Odin Project</a>.
+          A radiophysics student currently employed as Procurement Manager
+          passionately learning Web Development.
+          <br />
+          <br />
+          My journey started as a Back-End Dev student
+          of <a href="https://practicum.yandex.ru/">Yandex Practicum</a>.
+          I felt out of place since most of the students with whom I had
+          to cooperate for some tasks did not show same interest and motivation.
+          <br />
+          <br />
+          I decided to leave and proceed on my own.
+          My first personal pet project was ecommerce site built with Django.
+          However I lacked some fundamental knowledge of CSS, JS.
+          <br />
+          <br />
+          This is where I stumbled
+          across <a href="https://theodinproject.com">The Odin Project</a>.
+          This helped me so much in understanding Web Dev and I continue forward
+          in learning new things with it!
         </p>
         <div className="tech">
           {Object.keys(ICONS_LIST['tech']).map((item, index) => (
