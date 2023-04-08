7c239b3c5d89f3f007147da27ab907f3471a5b43
Nelfimov <nelfimov@mail.ru>
2023-04-08 11:01:31 +0300

    fixed height and width of footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,11 @@ import '../styles/Footer.css';
 const Footer = () => {
   return (
     <footer>
-      <a href="https://github.com/nelfimov">
-        <img src={ICONS_LIST.general.github} alt="github" />
-      </a>
+      <div className="container">
+        <a href="https://github.com/nelfimov">
+          <img src={ICONS_LIST.general.github} alt="github" />
+        </a>
+      </div>
     </footer>
   );
 };
